0e35e723c41d15e67a0d5eca8940e2e9c4966ebb
Simon Pfeiffer <simon-p-96@web.de>
2022-02-10 17:36:07 +0100

    Improved design

--- flask_app/forms.py
+++ flask_app/forms.py
@@ -5,7 +5,7 @@ from wtforms import SubmitField
 
 class SubmitReceiptForm(FlaskForm):
    
-    receipt = FileField("Possible inputs are jpeg, jpg or png files with less than 4mb. Make sure the receipt is well readable.",validators=[FileRequired(), FileAllowed(['jpeg','jpg','png', 'xlsx'],"Are you sure this was a jpg, jpeg or png file?")])
+    receipt = FileField("Please select an image.",validators=[FileRequired(), FileAllowed(['jpeg','jpg','png', 'xlsx'],"Are you sure this was a jpg, jpeg or png file?")])
 
     submit = SubmitField('Analyze receipt')
 
