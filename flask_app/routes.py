2be2345f3033c9a2dedca3b2ffe2d4aa5e469169
Simon Pfeiffer <simon-p-96@web.de>
2022-02-22 17:41:56 +0100

    Updated API

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -55,8 +55,16 @@ class AnalyzeReceipt(Resource):
         shower_eq = str(round(sum(category['footprint'])/196,2)).replace('.',',')
         
         
-        output = {"results": results["product"].to_list(), "footprint" : results["footprint"].to_list()}
-        print(output)
+        output = {
+            "results": results["product"].to_list(),
+            "footprint" : results["footprint"].to_list(),
+            "description" : results["description"].to_list(),
+            "quantity" : results["quantity"].to_list(),
+            "total" : results["total"].to_list(),
+            "typical_weight" : results["typical_weight"].to_list(),
+            "category" : results["category"].to_list()
+             }
+        #print(output)
         return output, 201
 
 api.add_resource(AnalyzeReceipt, '/Api')
@@ -123,75 +131,10 @@ def Home():
         # Calculate car equivalent
         car_eq = str(round(sum(category['footprint'])/250,2)).replace('.',',')
         shower_eq = str(round(sum(category['footprint'])/196,2)).replace('.',',')
-
+        print(results)
         pd.set_option('display.float_format','{:.0f}'.format)
         flash('Successfully analyzed receipt', 'success')
         return render_template('results.html', results = results.to_dict(orient='records'), filename = str(file.filename), image_path = url_for('static', filename=f'assets/{filename}'), script = script, div = div, total = total, car_eq = car_eq, shower_eq = shower_eq, form = form)
     return render_template('home.html', form = form)
 
 
-
-
-@app.route("/Api", methods=['POST'])
-def Api():
-    form = SubmitReceiptForm()
-    if form.validate_on_submit():
-        file = form.receipt.data
-        image = Image.open(file)
-        image = ImageOps.exif_transpose(image)
-        filename = secure_filename(file.filename)
-        assets_dir = os.path.join(os.path.dirname(app.instance_path),'flask_app' ,'static' ,'assets')    
-        image_path = os.path.join(assets_dir, filename)
-        image.save(image_path, format='JPEG', quality=40)
-        receipt_request = Receipt(receipt_file = image_path)
-        db.session.add(receipt_request)
-        db.session.commit()
-
-        # Load mapping table
-        grocery_mapping = pd.read_excel(os.path.join(os.path.dirname(app.instance_path), "grocery_mapping.xlsx"), engine="openpyxl")
-
-        if '.xlsx' in filename:
-            items = pd.read_excel(image_path)
-            results ,missed_item = match_and_merge(items,grocery_mapping,"description","product",75)
-
-        else:
-            """
-            # Old version without azure:
-            # Process the receipt
-            receipt = pre_processing(image_path)
-
-            # Ocr the receipt items
-            try:
-                ocr_result = ocr_receipt(receipt)
-            except ValueError:
-                flash(Markup('It seems like our algorithm was unable to recognize the receipts structure or content, feel free to send us the picture via mail to <a href="mailto:receipt@project-count.com">receipt@project-count.com</a> we will get back to you as soon as possible.'), 'danger')
-                return render_template('home.html', form = form)
-            """
-            ocr_result = azure_form_recognition(image_path)
-
-            # Match with footprint data
-            results, missed_item = match_and_merge(ocr_result,grocery_mapping,"description","product",75)
-            
-            results["request_id"] = Receipt.query.filter(Receipt.receipt_file == image_path).first().id
-            results.to_sql(name="results",con=db.engine, index=False, if_exists="append")
-
-        # Output missed items
-
-        writer2 = pd.ExcelWriter("not_recognized.xlsx", engine="openpyxl", mode="a", if_sheet_exists="replace")
-        missed_item.to_excel(writer2,index=False, sheet_name=filename.split(".",1)[0])
-        writer2.save()
-
-        # Calculate category percentages
-        category = results.groupby('category').agg({'footprint': 'sum'})
-
-        # Get pie chart
-        script, div = prepare_pie(category)
-        
-        # Calculate total
-        total = str(sum(category['footprint'])/1000).replace('.',',')
-
-        # Calculate car equivalent
-        car_eq = str(round(sum(category['footprint'])/250,2)).replace('.',',')
-        shower_eq = str(round(sum(category['footprint'])/196,2)).replace('.',',')
-
-    return results.to_dict(orient='records') 
